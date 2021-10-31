005a553a77a2516d9eae8c0d526a0c86a2015c02
LoginALee <alejandrodxwwe@hotmail.com>
2021-10-31 11:33:55 -0600

    Add username to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_171055) do
+ActiveRecord::Schema.define(version: 2021_10_31_173048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_10_31_171055) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
