3c6c866c69367079abe9bb365e31af94d58a8780
LoginALee <alejandrodxwwe@hotmail.com>
2022-07-04 09:46:06 -0500

    feature: add show for todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,14 +4,21 @@ class TodosController < ApplicationController
     render json: todos
   end
 
+  def show
+    todo = Todo.find(params[:id])
+    render json: todo
+  end
+
   def create
     todo = Todo.create(todo_params)
+    todo.user = logged_in_user
+    todo.save!
     render json: todo
   end
 
   def update
     todo = Todo.find(params[:id])
-    todo.update_attirbutes(todo_params)
+    todo.update(todo_params)
     render json: todo
   end
 
