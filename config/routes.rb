ef45105af97cdea6f04c5a45e26a594a769c4eb6
LoginALee <alejandrodxwwe@hotmail.com>
2022-08-25 12:58:03 -0500

    add namespace to api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resource :users, only: [:create]
-  post "/login", to: "users#login"
-  get "/auto_login", to: "users#auto_login"
-  resources :todos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resource :users, only: [:create]
+      post "/login", to: "users#login"
+      get "/auto_login", to: "users#auto_login"
+      resources :todos
+    end
+  end 
 end
