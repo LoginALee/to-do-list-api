d0e297673c50204d4f015c26720f9d476d8e45c4
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2023-01-12 14:21:08 -0600

    fix: add prod link to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://to-do-list-loginalee.vercel.app/'
 
     resource '*',
              headers: :any,
